feff7e52cb2656c1999b86b162e9405af250bc1c
hseyinnnng33 <110617469+hseyinnnng33@users.noreply.github.com>
2022-11-01 16:19:07 +0300

    Add files via upload

---
+++ c0208/ft_strlowcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+/*büyük harfi küçük harfe çevirme*/
+int	main()
+{
+	char str[] = "ASDADA";
+	printf("%s", ft_strupcase(str));
+}

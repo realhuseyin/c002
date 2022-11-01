c2cdef28f8bf0e1cfd8c2ad155ffbdb9c42734f1
hseyinnnng33 <110617469+hseyinnnng33@users.noreply.github.com>
2022-11-01 16:17:42 +0300

    Add files via upload

---
+++ c0203/ft_str_is_numeric.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		/*0 dan büyük  9 dan küçük ve degerini döndür, i arttır*/
+		if (!(str[i] >= 48 && str [i] <= 57))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main()
+{
+	printf("%d", ft_str_is_numeric("01948987493"));
+	printf("\n%d", ft_str_is_numeric("0484882j8995489"));
+	printf("\n%d", ft_str_is_numeric("-_jsdsdc67sdlkc,"));
+}
